10a10c6bc39bae1be40f91d8fa7e4296a1482c32
mschute <46330682+mschute@users.noreply.github.com>
2024-07-13 18:14:11 +0100

    Added item to be included in Settings

--- STATS/Settings.swift
+++ STATS/Settings.swift
@@ -5,6 +5,11 @@ struct Settings: View {
         Text("Settings")
             .font(.largeTitle)
         //TODO: Need to add a List with setting options sections
+        
+//        Form {
+//            //TODO: Have Manage Tag section to Add, Edit and Delete - this needs to navigate to new page
+//            Text("Manage Tags")
+//        }
     }
 }
 
