014045175664164f0ecd1494f5583df3f6cae86b
mschute <46330682+mschute@users.noreply.github.com>
2024-07-19 11:22:23 +0100

    Updated strategy and added filter
    Updated the data flow stragegy, utilising @Query so the list is dynamically updating without passing in two copies of the list. Added a filter through computed properties.

--- STATS/Stat/StatList.swift
+++ STATS/Stat/StatList.swift
@@ -4,7 +4,11 @@ import SwiftUI
 struct StatList: View {
     @Environment(\.modelContext) private var modelContext
     
+    @Query(sort: \Category.name) var categories: [Category]
+    
     @Binding var stats: [AnyStat]
+
+    @Binding var filter: String?
     
     @State private var isCreatedAscending = false
     @State private var isNameAscending = false
@@ -15,23 +19,35 @@ struct StatList: View {
                 StatUtility.Card(stat: item.stat)
             }
             .onDelete(perform: deleteItems)
-            
         }
         .navigationTitle("Stat List")
         .toolbar {
-            ToolbarItem{
-                Menu {
-                    Button(action: { sortStats(sortType: .created) } ) {
-                        Label("Sort by Date", systemImage: "calendar")
+            ToolbarItem {
+                HStack {
+                    Menu {
+                        Button(action: { sortStats(sortType: .created) } ) {
+                            Label("Sort by Date", systemImage: "calendar")
+                        }
+                        
+                        Button(action: { sortStats(sortType: .name) } ) {
+                            Label("Sort by Name", systemImage: "abc")
+                        }
+                    } label: {
+                        Label("", systemImage: "arrow.up.arrow.down")
                     }
                     
-                    Button(action: { sortStats(sortType: .name) } ) {
-                        Label("Sort by Name", systemImage: "abc")
+                    Menu {
+                        ForEach(categories, id: \.id){ category in
+                            if (filter == "\(category.name)") {
+                                Button("\(category.name)", systemImage: "checkmark", action: { filter = nil } )
+                            } else {
+                                Button("\(category.name)", action: { filter = category.name } )
+                            }
+                        }
+                    } label: {
+                        Label("", systemImage: "line.3.horizontal.decrease.circle")
                     }
-                } label: {
-                    Label("", systemImage: "arrow.up.arrow.down")
                 }
-                //TODO: Implement filter? After adding tag to Model properties
             }
         }
     }
@@ -39,15 +55,13 @@ struct StatList: View {
     func deleteItems(offsets: IndexSet) {
         withAnimation {
             // Uses IndexSet to remove from [AnyStat] and ModelContext
-            // '&' means its passed by reference
-            StatUtility.Remove(offsets: offsets, statItems: &stats, modelContext: modelContext)
+            StatUtility.Remove(offsets: offsets, statItems: stats, modelContext: modelContext)
         }
     }
     
-    func sortStats(sortType: SortType){
+    func sortStats(sortType: SortType) {
         
         switch sortType {
-        
         case .created where isCreatedAscending == false:
             stats.sort { $0.stat.created < $1.stat.created}
             isCreatedAscending = true
@@ -62,4 +76,5 @@ struct StatList: View {
             isNameAscending = false
         }
     }
+    
 }
