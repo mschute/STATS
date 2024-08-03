683b49fa9725d995aa9950c1cd177417ac4ea0fd
mschute <46330682+mschute@users.noreply.github.com>
2024-08-03 11:11:59 +0100

    Adjusted look of Splash Screen

--- STATS/Stat/SplashView.swift
+++ STATS/Stat/SplashView.swift
@@ -4,16 +4,19 @@ struct SplashView: View {
     
     var body: some View {
         ZStack {
-            Color.black
+            Color.background
                 .edgesIgnoringSafeArea(.all)
             
-            Text("STATS")
-                .font(.largeTitle)
-                .foregroundColor(.white)
+            VStack(spacing: 40) {
+                Image(systemName: "chart.bar.xaxis")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                Text("STATS")
+                    .font(.custom("Menlo", size: 100))
+            }
+            .foregroundColor(.white)
         }
     }
-}
-
-#Preview {
-    SplashView()
 }
