87dfc28a6a2cdc2ab5a2cbadc80b2461088da4af
mschute <46330682+mschute@users.noreply.github.com>
2024-08-07 16:35:34 +0100

    Clean up

--- STATS/Stat/History.swift
+++ STATS/Stat/History.swift
@@ -3,30 +3,28 @@ import SwiftUI
 
 // https://www.youtube.com/watch?v=Saw_sZWa4aQ
 struct History: View {
+    private var stat: any Stat
     @State var startDate: Date = Date()
     @State var endDate: Date = Date()
     @State var dateRange: (startDate: Date, endDate: Date) = (Date(), Date())
-    
-    private var stat: any Stat
     
     //https://stackoverflow.com/questions/77039981/swiftdata-query-with-predicate-on-relationship-model?ref=simplykyra.com
     //https://developer.apple.com/documentation/swiftdata/filtering-and-sorting-persistent-data
     init(stat: any Stat) {
         self.stat = stat
-        
         _dateRange = State(initialValue: AnyStat.getEntryDateRange(entryArray: stat.statEntry))
         _startDate = State(initialValue: dateRange.startDate)
         _endDate = State(initialValue: dateRange.endDate)
     }
 
     var body: some View {
-        VStack {
-            Text("History")
-                .font(.largeTitle)
-            
-            DateRangePicker(startDate: $startDate, endDate: $endDate)
-            
-            StatUtility.EntryList(stat: stat, startDate: $startDate, endDate: $endDate)
+        VStack(spacing: 20) {
+            Form {
+                Section {
+                    DateRangePicker(startDate: $startDate, endDate: $endDate)
+                }
+                StatUtility.EntryList(stat: stat, startDate: $startDate, endDate: $endDate)
+            }
         }
         //Update date range to fix bug of entry not showing if timestamp is edited
         .onAppear {
