4861b5c5beff84d059aad88432bad37af9f2e205
mschute <46330682+mschute@users.noreply.github.com>
2024-08-09 20:52:33 +0100

    Separated Decimal Form in Add and Edit Forms

---
+++ STATS/ViewModifiers/HideKeyboardGestureModifier.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+//Keyboard implementation: https://stackoverflow.com/questions/56491386/how-to-hide-keyboard-when-using-swiftui#comment109419055_60010955
+struct HideKeyboardGestureModifier: ViewModifier {
+    @Environment(\.keyboardIsShown) var keyboardIsShown
+
+    
+    func body(content: Content) -> some View {
+        content
+            .gesture(TapGesture().onEnded {
+                UIApplication.shared.resignCurrentResponder()
+            }, including: keyboardIsShown ? .all : .none)
+    }
+}
+
+extension UIApplication {
+    func resignCurrentResponder() {
+        sendAction(#selector(UIResponder.resignFirstResponder),
+                   to: nil, from: nil, for: nil)
+    }
+}
+
+extension View {
+    /// Assigns a tap gesture that dismisses the first responder only when the keyboard is visible to the KeyboardIsShown EnvironmentKey
+    func dismissKeyboardOnTap() -> some View {
+        modifier(HideKeyboardGestureModifier())
+    }
+    
+    /// Shortcut to close in a function call
+    func resignCurrentResponder() {
+        UIApplication.shared.resignCurrentResponder()
+    }
+}
+
+extension EnvironmentValues {
+    var keyboardIsShown: Bool {
+        get { return self[KeyboardIsShownEVK.self] }
+        set { self[KeyboardIsShownEVK.self] = newValue }
+    }
+}
+
+private struct KeyboardIsShownEVK: EnvironmentKey {
+    static let defaultValue: Bool = false
+}
