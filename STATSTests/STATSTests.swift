913a8e5d71842041871afae7654f9a52a3afd1c8
mschute <46330682+mschute@users.noreply.github.com>
2024-08-12 15:38:10 +0100

    Added note

--- STATSTests/STATSTests.swift
+++ STATSTests/STATSTests.swift
@@ -8,6 +8,7 @@
 import XCTest
 @testable import STATS
 
+//Basics of unit testing: https://www.youtube.com/watch?v=opkU2UuPk0A
 final class STATSTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -24,6 +25,10 @@ final class STATSTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        //Arrange
+        //Act
+        //Assert
     }
 
     func testPerformanceExample() throws {
