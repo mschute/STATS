ac0aa0c13e9b1158182b81165db0e266fe055b62
mschute <46330682+mschute@users.noreply.github.com>
2024-08-12 18:06:45 +0100

    Fixed color mode bug

--- STATS/Extensions/ColorMode.swift
+++ STATS/Extensions/ColorMode.swift
@@ -7,7 +7,6 @@ import SwiftUI
 extension UIApplication {
     var keyWindow: UIWindow? {
         return self.connectedScenes
-            .filter { $0.activationState == .foregroundActive }
             .first(where: { $0 is UIWindowScene })
             .flatMap({ $0 as? UIWindowScene})?.windows
             .first(where: \.isKeyWindow)
