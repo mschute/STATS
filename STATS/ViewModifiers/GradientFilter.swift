07ccd3dad848e08bcd4e7868119fbea3ee319ae0
mschute <46330682+mschute@users.noreply.github.com>
2024-08-12 15:38:02 +0100

    Updated look of gradient filter

--- STATS/ViewModifiers/GradientFilter.swift
+++ STATS/ViewModifiers/GradientFilter.swift
@@ -14,10 +14,6 @@ struct GradientFilter: ViewModifier {
             .background(LinearGradient(gradient: Gradient(colors: [gradientColor, gradientHighlight]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
             .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
-            .overlay(
-            RoundedRectangle(cornerRadius: cornerRadius)
-                .stroke(lineHighlight.opacity(0.3), lineWidth: 1)
-            )
             .shadow(color: Color(.counterHighlight).opacity(0.4), radius: 10, x: 0, y: 5)
     }
 }
