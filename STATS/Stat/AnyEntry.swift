17c3404d631465028c60875b6c57e8d6ef91a2c4
mschute <46330682+mschute@users.noreply.github.com>
2024-08-23 09:09:29 +0100

    Moved function to more appropriate location

--- STATS/Stat/AnyEntry.swift
+++ STATS/Stat/AnyEntry.swift
@@ -7,3 +7,25 @@ class AnyEntry: Identifiable {
         self.entry = entry
     }
 }
+
+extension AnyEntry {
+    static func getEntryDateRange(entryArray: [any Entry]) -> (startDate: Date, endDate: Date) {
+        
+        guard let firstEntry = entryArray.first else {
+            return (Date(), Date())
+        }
+        
+        var startDate = firstEntry.timestamp
+        var endDate = firstEntry.timestamp
+        
+        for entry in entryArray {
+            if(startDate > entry.timestamp) {
+                startDate = entry.timestamp
+            } else if(endDate < entry.timestamp ) {
+                endDate = entry.timestamp
+            }
+        }
+        
+        return (startDate, endDate)
+    }
+}
--- STATS/Stat/AnyStat.swift
+++ STATS/Stat/AnyStat.swift
@@ -20,28 +20,8 @@ class AnyStat: Identifiable, Hashable {
 }
 
 extension AnyStat {
-    static func getEntryDateRange(entryArray: [any Entry]) -> (startDate: Date, endDate: Date) {
-        
-        guard let firstEntry = entryArray.first else {
-            return (Date(), Date())
-        }
-        
-        var startDate = firstEntry.timestamp
-        var endDate = firstEntry.timestamp
-        
-        for entry in entryArray {
-            if(startDate > entry.timestamp) {
-                startDate = entry.timestamp
-            } else if(endDate < entry.timestamp ) {
-                endDate = entry.timestamp
-            }
-        }
-        
-        return (startDate, endDate)
-    }
-    
     static func updateDateRange(stat: any Stat, dateRange: inout (startDate: Date, endDate: Date), startDate: inout Date, endDate: inout Date) {
-        dateRange = getEntryDateRange(entryArray: stat.statEntry)
+        dateRange = AnyEntry.getEntryDateRange(entryArray: stat.statEntry)
         startDate = dateRange.startDate
         endDate = dateRange.endDate
     }
