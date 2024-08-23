9c6b7ef96141b969a8773f64e2d953571ec9c3a3
mschute <46330682+mschute@users.noreply.github.com>
2024-08-23 09:07:38 +0100

    Added comments

--- STATS/Camera/UIKitCamera.swift
+++ STATS/Camera/UIKitCamera.swift
@@ -1,19 +1,24 @@
 import SwiftUI
 
 //https://www.youtube.com/watch?v=1ZYE5FcUN4Y&list=PLBn01m5Vbs4DLU9Yiff2V8oyslCdB-pnj&index=3
+//UIViewControllerRepresentable allows a SwiftUI view to wrap a UIKit view Controller
 struct UIKitCamera: UIViewControllerRepresentable {
+    //Allows the parent view to be notified when a picture is captured
     @Binding var selectedImage: UIImage?
     @Environment(\.dismiss) var dismiss
 //https://developer.apple.com/documentation/uikit/uiimagepickercontroller/1619113-cameraoverlayview
+    //Allow an overlay over the camera view
     var overlayView: UIView? = CameraOverlay()
     
     //Customise control: https://developer.apple.com/documentation/uikit/uiimagepickercontroller/customizing_an_image_picker_controller
+    //Creates and configures UIImagePickerController
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let imagePicker = UIImagePickerController()
         imagePicker.allowsEditing = true
         imagePicker.sourceType = .camera
         imagePicker.delegate = context.coordinator
         
+        //Assign overlay to preview
         if imagePicker.sourceType == .camera {
             if let overlayView = overlayView {
                 overlayView.frame = calculateCameraPreviewFrame(picker: imagePicker)
@@ -24,6 +29,7 @@ struct UIKitCamera: UIViewControllerRepresentable {
         return imagePicker
     }
     
+    //Calculates the cameras preview for the overlay
     func calculateCameraPreviewFrame(picker: UIImagePickerController) -> CGRect {
         //Get screen size: https://designcode.io/swiftui-handbook-detect-screen-size
         let fullScreenSize = UIScreen.main.bounds.size
@@ -38,20 +44,24 @@ struct UIKitCamera: UIViewControllerRepresentable {
         return previewFrame
     }
     
+    //Function is required by UiViewControllerRepresentable
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         
     }
     
+    //Handles delegate methods of UIImagePickerController
     func makeCoordinator() -> Coordinator {
         Coordinator(parent: self)
     }
     
+    //Acts as a delegate for UIImagePickerController, handles user interactions when image is selected or camera is dismissed
     final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
         var parent: UIKitCamera
         init(parent: UIKitCamera) {
             self.parent = parent
         }
         
+        //Called when user picks an image
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                 parent.selectedImage = image
