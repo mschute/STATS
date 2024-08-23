25b67de64d8c5207e0edab0ebec61f5ca5709f69
mschute <46330682+mschute@users.noreply.github.com>
2024-08-23 09:07:18 +0100

    Adjusted name

--- STATS/Extensions/HideKeyboard.swift
+++ STATS/Extensions/HideKeyboard.swift
@@ -1,9 +1,8 @@
 import SwiftUI
 
 //Keyboard implementation: https://stackoverflow.com/questions/56491386/how-to-hide-keyboard-when-using-swiftui#comment109419055_60010955
-struct HideKeyboardGestureModifier: ViewModifier {
+struct HideKeyboard: ViewModifier {
     @Environment(\.keyboardIsShown) var keyboardIsShown
-
     
     func body(content: Content) -> some View {
         content
@@ -22,8 +21,8 @@ extension UIApplication {
 
 extension View {
     // Assign a tap gesture that dismisses the first responser only when the keyboard is visible to the KeyboardIsShown EnvironmentKey
-    func dismissKeyboardOnTap() -> some View {
-        modifier(HideKeyboardGestureModifier())
+    func dismissKeyboard() -> some View {
+        modifier(HideKeyboard())
     }
     
     // Shortcut to close in a function call
