7ab9e0a8f117e0c474af66939e6210e375569f01
mschute <46330682+mschute@users.noreply.github.com>
2024-08-26 16:02:15 +0100

    Added citation

--- STATS/Stat/DateUtility.swift
+++ STATS/Stat/DateUtility.swift
@@ -7,11 +7,13 @@ struct DateUtility {
         return formatter.string(from: date)
     }
     
-    static func startOfDay(for date: Date) -> Date {
+    static func startOfDay(date: Date) -> Date {
         return Calendar.current.startOfDay(for: date)
     }
     
-    static func endOfDay(for date: Date) -> Date {
+    //To find end of day: https://www.reddit.com/r/swift/comments/16q6zrj/why_swift_didnt_provide/
+    //Subtract 1 second: https://developer.apple.com/documentation/foundation/date/1948745-addingtimeinterval
+    static func endOfDay(date: Date) -> Date {
         let calendar = Calendar.current
         let startOfDay = calendar.startOfDay(for: date)
         return calendar.date(byAdding: .day, value: 1, to: startOfDay)!.addingTimeInterval(-1)
