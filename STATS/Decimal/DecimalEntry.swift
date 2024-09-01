d9feaa917a15ad868da9b6be51e52642e08426e2
mschute <46330682+mschute@users.noreply.github.com>
2024-09-01 23:17:37 +0100

    Removed entry from decimal

--- STATS/Decimal/DecimalEntry.swift
+++ STATS/Decimal/DecimalEntry.swift
@@ -4,15 +4,13 @@ import SwiftUI
 
 @Model
 final class DecimalEntry: Entry, Identifiable {
-    var entryId: UUID
     var timestamp: Date
     var value: Double
     var note: String
     //Associated relationship must be optional / does not need @Relationship
     var stat: DecimalStat?
     
-    init(entryId: UUID = UUID(), timestamp: Date = Date(), value: Double = 0.0, note: String = "") {
-        self.entryId = entryId
+    init(timestamp: Date = Date(), value: Double = 0.0, note: String = "") {
         self.timestamp = timestamp
         self.value = value
         self.note = note
