2865c3ff65a1a262895b162943f5ecf6261363a6
mschute <46330682+mschute@users.noreply.github.com>
2024-09-06 20:07:33 +0100

    Remove unnecessary group

--- STATS/Navigation/Navbar.swift
+++ STATS/Navigation/Navbar.swift
@@ -6,35 +6,33 @@ struct Navbar: View {
     
     var body: some View {
         TabView(selection: $selectedTab.selectedTab) {
-            Group {
-                NavigationStack {
-                    ContentView()
-                }
-                .tabItem {
-                        Image(systemName: "list.bullet.circle.fill")
-                        Text("Stat List")
-                    
-                }
-                .tag(Tab.statList)
-                
-                NavigationStack {
-                    ChooseStatType()
-                }
-                .tabItem {
-                        Image(systemName: "plus.circle.fill")
-                        Text("New Stat")
-                }
-                .tag(Tab.addStat)
-                
-                NavigationStack {
-                    Settings()
-                }
-                .tabItem {
-                        Image(systemName: "gearshape.circle.fill")
-                        Text("Settings")
-                }
-                .tag(Tab.settings)
+            NavigationStack {
+                ContentView()
             }
+            .tabItem {
+                Image(systemName: "list.bullet.circle.fill")
+                Text("Stat List")
+                
+            }
+            .tag(Tab.statList)
+            
+            NavigationStack {
+                ChooseStatType()
+            }
+            .tabItem {
+                Image(systemName: "plus.circle.fill")
+                Text("New Stat")
+            }
+            .tag(Tab.addStat)
+            
+            NavigationStack {
+                Settings()
+            }
+            .tabItem {
+                Image(systemName: "gearshape.circle.fill")
+                Text("Settings")
+            }
+            .tag(Tab.settings)
         }
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
