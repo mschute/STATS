34b21f35be0fd695c97da4b9a8f94808f8fb1b6f
mschute <46330682+mschute@users.noreply.github.com>
2024-09-06 19:10:28 +0100

    Simplified NavLink

--- STATS/Stat/ChooseStatType.swift
+++ STATS/Stat/ChooseStatType.swift
@@ -8,9 +8,7 @@ struct ChooseStatType: View {
             TopBar(title: "NEW STAT", topPadding: 60, bottomPadding: 20)
             Spacer()
             VStack(spacing: 40) {
-                NavigationLink {
-                    CounterFormAdd()
-                } label: {
+                NavigationLink(destination: CounterFormAdd()) {
                     Text("Counter")
                         .textButtonStyle(fontSize: 20, verticalPadding: 20, horizontalPadding: 50, align: .center, statColor: .cyan, statHighlightColor: .counterHighlight)
                 }
@@ -21,9 +19,7 @@ struct ChooseStatType: View {
                         }
                 )
                 
-                NavigationLink {
-                    DecimalFormAdd()
-                } label: {
+                NavigationLink(destination: DecimalFormAdd()) {
                     Text("Decimal")
                         .textButtonStyle(fontSize: 20, verticalPadding: 20, horizontalPadding: 50, align: .center, statColor: .mint, statHighlightColor: .decimalHighlight)
                 }
@@ -34,9 +30,7 @@ struct ChooseStatType: View {
                         }
                 )
                 
-                NavigationLink {
-                    PictureFormAdd()
-                } label: {
+                NavigationLink(destination: PictureFormAdd()) {
                     Text("Picture")
                         .font(.custom("Menlo", size: 20))
                         .textButtonStyle(fontSize: 20, verticalPadding: 20, horizontalPadding: 50, align: .center, statColor: .teal, statHighlightColor: .pictureHighlight)
