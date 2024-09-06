76482057d206ac72453086784ded47964c201b05
mschute <46330682+mschute@users.noreply.github.com>
2024-09-06 19:10:08 +0100

    Removed unnecessary field and associated code

--- STATS/Picture/PictureEntryForm.swift
+++ STATS/Picture/PictureEntryForm.swift
@@ -9,12 +9,10 @@ struct PictureEntryForm: View {
     
     @State private var timestamp: Date = Date()
     @State private var note: String = ""
-    @State private var image: Data?
     
     @State private var selectedPhoto: PhotosPickerItem?
-    @State private var selectedPhotoData: Data?
-    
     @State private var cameraImage: UIImage?
+    @State private var selectedPhotoData: Data?
     @State private var showCamera: Bool = false
     
     @State private var showAlert: Bool = false
@@ -41,10 +39,10 @@ struct PictureEntryForm: View {
                     .simultaneousGesture(
                         TapGesture()
                             .onEnded { _ in
-                                if image == nil {
+                                if selectedPhotoData == nil {
                                     showAlert = true
                                 } else {
-                                    PictureEntry.addEntry(pictureStat: pictureStat, timestamp: timestamp, note: note, image: image, modelContext: modelContext)
+                                    PictureEntry.addEntry(pictureStat: pictureStat, timestamp: timestamp, note: note, image: selectedPhotoData, modelContext: modelContext)
                                     selectedDetailTab.selectedDetailTab = .history
                                 }
                                 Haptics.shared.play(.light)
@@ -53,16 +51,10 @@ struct PictureEntryForm: View {
             }
         }
         .dismissKeyboard()
+        //TODO: Refactor this into a separate function? Repeated in entry
         .task(id: selectedPhoto) {
             if let data = try? await selectedPhoto?.loadTransferable(type: Data.self){
-                image = data
-            }
-        }
-        .onChange(of: selectedPhotoData) {
-            if let data = selectedPhotoData {
-                image = data
-            } else {
-                image = nil
+                selectedPhotoData = data
             }
         }
         //Alerts: https://www.hackingwithswift.com/quick-start/swiftui/how-to-show-an-alert
